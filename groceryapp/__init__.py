1246e6b834463f1c1f59c407e4785298bf7ce725
Sven Mark Hofstra <svenmark10@gmail.com>
2019-06-22 14:40:07 +0200

    Service account in env

--- groceryapp/__init__.py
+++ groceryapp/__init__.py
@@ -2,8 +2,10 @@ import firebase_admin
 from firebase_admin import credentials
 import os
 
-if os.environ.get('SERVICE_ACCOUNT_KEY'):
-    cred = credentials.Certificate(os.environ.get('SERVICE_ACCOUNT_KEY'))
+print(os.environ.get('SERVICE_ACCOUNT_KEY'))
+
+if os.environ.get('SERVICE_ACCOUNT_KEY') is not None:
+    cred = credentials.Certificate(str(os.environ.get("SERVICE_ACCOUNT_KEY")))
 else:
     cred = credentials.Certificate("secrets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)

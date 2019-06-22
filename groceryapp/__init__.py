acc54f140ad3483405e8355dbf209f8c04e71be1
Sven Mark Hofstra <svenmark10@gmail.com>
2019-06-22 14:45:18 +0200

    Check all envs

--- groceryapp/__init__.py
+++ groceryapp/__init__.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 import os
 
 print(os.environ.get('SERVICE_ACCOUNT_KEY'))
+print(os.environ)
 
 if os.environ.get('SERVICE_ACCOUNT_KEY') is not None:
     cred = credentials.Certificate(str(os.environ.get("SERVICE_ACCOUNT_KEY")))

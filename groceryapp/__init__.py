284ad2321eea847e4c45a1afb22cf1bb58bbfe47
Sven Mark Hofstra <svenmark10@gmail.com>
2019-06-22 16:46:36 +0200

    Remove str conversion

--- groceryapp/__init__.py
+++ groceryapp/__init__.py
@@ -6,7 +6,7 @@ print(os.environ)
 print(os.environ.get('SERVICE_ACCOUNT_KEY'))
 
 if os.environ.get('SERVICE_ACCOUNT_KEY') is not None:
-    cred = credentials.Certificate(str(os.environ.get("SERVICE_ACCOUNT_KEY")))
+    cred = credentials.Certificate(os.environ.get("SERVICE_ACCOUNT_KEY"))
 else:
     cred = credentials.Certificate("secrets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)

7a778ed4831f0f5f5375cb3b63f6319415f04069
Sven Mark Hofstra <svenmark10@gmail.com>
2019-06-22 17:28:25 +0200

    Import account key and load it with json

--- groceryapp/__init__.py
+++ groceryapp/__init__.py
@@ -7,7 +7,7 @@ print(os.environ)
 print(os.environ.get('SERVICE_ACCOUNT_KEY'))
 
 if os.environ.get('SERVICE_ACCOUNT_KEY') is not None:
-    cred = credentials.Certificate(dict(json.dumps(os.environ.get("SERVICE_ACCOUNT_KEY"))))
+    cred = credentials.Certificate(dict(json.loads(os.environ.get("SERVICE_ACCOUNT_KEY"))))
 else:
     cred = credentials.Certificate("secrets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
